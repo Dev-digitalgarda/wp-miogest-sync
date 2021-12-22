0d4332b70a195ed1ac275034b5cd5bcc9a95bd51
euberdeveloper <euberdeveloper@gmail.com>
2021-12-22 19:30:18 +0100

    feat(syncer): add better log with also echo
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/utils/Logger.php
+++ src/utils/Logger.php
@@ -7,7 +7,20 @@ use Monolog;
 $__wp_miogest_sync_logger = new Monolog\Logger('wp-miogest-sync');
 $__wp_miogest_sync_logger->pushHandler(new Monolog\Handler\StreamHandler('wp-miogest-sync.log'));
 
-class Logger {
+class Logger
+{
     public static ?Monolog\Logger $logger = null;
+
+    public static function info(string $message)
+    {
+        self::$logger->info($message);
+        echo $message . "<br />";
+    }
+
+    public static function warning(string $message)
+    {
+        self::$logger->warning($message);
+        echo $message . "<br />";
+    }
 }
 Logger::$logger = $__wp_miogest_sync_logger;
